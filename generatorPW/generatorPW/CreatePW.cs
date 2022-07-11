8dda068fb1312c69ed90063da4de6d703dec187e
Francesco-Vitolo <94391020+Francesco-Vitolo@users.noreply.github.com>
2022-07-11 13:03:04 +0200

    Add files via upload

---
+++ generatorPW/generatorPW/CreatePW.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generatorPW
+{
+    public static class CreatePW
+    {
+        public static string Password { get; private set; }
+
+        private const string UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string LowerCase = "abcdefghijklmnopqrstuvwxyz";
+        private const string Sonderzeichen = "!.,_-#'+-*/()[]{}";
+        private static readonly Random Rand = new Random();
+        public static string Create(int length)
+        {
+            Password = "";
+            for (int i = 0; i < length; i++)
+            {
+                switch(Rand.Next(1, 4))
+                {
+                    case 1:
+                        Password += UpperCase[Rand.Next(0, 26)];
+                        break;
+                    case 2:
+                        Password += LowerCase[Rand.Next(0, 26)];
+                        break;
+                    case 3:
+                        Password += Sonderzeichen[Rand.Next(0, Sonderzeichen.Length)];
+                        break;
+                }
+            }
+            return Password;
+        }
+    }
+}
---
+++ generatorPW/generatorPW/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace generatorPW
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string TickValue { get; private set; }
+        public string Password { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            tb_length.DataContext = TickValue;
+            tb_pw.DataContext = Password;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Password = CreatePW.Create(int.Parse(TickValue));
+            tb_pw.DataContext = null;
+            tb_pw.DataContext = Password;
+
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TickValue = slider.Value.ToString();
+            tb_length.DataContext = null;
+            tb_length.DataContext = TickValue;
+        }
+    }
+}
